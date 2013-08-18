805cc06c5ac3c98a05f8bd72f7d7da01999bc31d
Ben-G <Benjamin.Encz@gmail.com>
2013-08-18 14:35:50 +0200

    implemented demo layer for scoreboard

--- Projectfiles/Components/ScoreboardDemoLayer.m
+++ Projectfiles/Components/ScoreboardDemoLayer.m
@@ -8,19 +8,44 @@
 
 #import "ScoreboardDemoLayer.h"
 
-@implementation ScoreboardDemoLayer
+@implementation ScoreboardDemoLayer {
+    ScoreboardEntryNode *scoreBoard;
+}
 
-- (id)init {
+- (id)init
+{
     self = [super init];
     
     if (self)
     {
-        ScoreboardEntryNode *scoreBoard = [[ScoreboardEntryNode alloc] initWithScoreImage:@"coin.png" fontFile:@"avenir.fnt"];
+        scoreBoard = [[ScoreboardEntryNode alloc] initWithScoreImage:@"coin.png" fontFile:@"avenir.fnt"];
         [self addChild:scoreBoard];
         scoreBoard.position = ccp(8, self.contentSize.height - 20);
+                
+        CCLabelTTF *backLabel = [CCLabelTTF labelWithString:@"Back" fontName:@"Arial" fontSize:14];
+        CCMenuItemLabel *backItem = [CCMenuItemLabel itemWithLabel:backLabel target:self selector:@selector(back)];
+        
+        CCMenu *menu = [CCMenu menuWithItems:backItem, nil];
+        [menu alignItemsVertically];
+        
+        [self performSelector:@selector(changePoints) withObject:nil afterDelay:0.5f];
+        
+        [self addChild:menu];
     }
     
     return self;
+}
+
+- (void)back
+{
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+    [[CCDirector sharedDirector] popScene];
+}
+
+- (void)changePoints
+{
+    [scoreBoard setScore:(scoreBoard.score + 20) animated:TRUE];
+    [self performSelector:@selector(changePoints) withObject:nil afterDelay:0.5f];
 }
 
 @end
