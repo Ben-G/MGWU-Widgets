d95acc51c49259c3df6d4653c4c4273f807b23a0
Ben-G <Benjamin.Encz@gmail.com>
2013-12-02 15:16:56 -0800

    added more examples for progress bars

--- Projectfiles/Components/MGWUProgressBarDemoLayer.m
+++ Projectfiles/Components/MGWUProgressBarDemoLayer.m
@@ -10,7 +10,9 @@
 #import "MGWUProgressBar.h"
 
 @implementation MGWUProgressBarDemoLayer {
-    MGWUProgressBar *progressBar;
+    MGWUProgressBar *progressBar1;
+    MGWUProgressBar *progressBar2;
+    MGWUProgressBar *progressBar3;
 }
 
 - (id)init
@@ -19,6 +21,31 @@
     
     if (self)
     {
+        // setting up a progress bar with a solid color
+        progressBar1 = [MGWUProgressBar progressBarWithStyle:MGWUProgressBarStyleHorizontal fillingColor:ccc4f(1.f, 0, 0, 1.f)];
+        progressBar1.position = ccp(50, (self.contentSize.height/4) * 3);
+        progressBar1.maximumSize = CGSizeMake(self.contentSize.width - 2 * progressBar1.position.x, 50);
+        progressBar1.maximumValue = 100.f;
+        progressBar1.changeValueAnimated = TRUE;
+        [self addChild:progressBar1];
+        
+        
+        // setting up a progress bar with a 9-patch image
+        progressBar2 = [MGWUProgressBar progressBarWithStyle:MGWUProgressBarStyleHorizontal fillingImage:@"notification-box.png" capInsets:CGRectMake(1, 1, 13, 48)];
+        progressBar2.position = ccp(50, (self.contentSize.height/4) * 2);
+        progressBar2.maximumSize = CGSizeMake(self.contentSize.width - 2 * progressBar1.position.x, 50);
+        progressBar2.maximumValue = 100.f;
+        progressBar2.changeValueAnimated = TRUE;
+        [self addChild:progressBar2];
+
+    
+        // setting up a progress bar with a sprite that will be revealed gradually
+        progressBar3 = [MGWUProgressBar progressBarWithStyle:MGWUProgressBarStyleHorizontal revealingSprite:@"metercolors.png"];
+        progressBar3.position = ccp(50, (self.contentSize.height/4) * 1);
+        progressBar3.maximumValue = 100.f;
+        progressBar3.changeValueAnimated = TRUE;
+        [self addChild:progressBar3];
+        
         CCLabelTTF *fillProgressBarLabel = [CCLabelTTF labelWithString:@"Fill ProgressBar" fontName:@"Arial" fontSize:14];
         CCMenuItemLabel *fillProgressBarMenuItem = [CCMenuItemLabel itemWithLabel:fillProgressBarLabel target:self selector:@selector(fillProgressBar)];
         
@@ -31,22 +58,9 @@
         CCMenu *menu = [CCMenu menuWithItems:fillProgressBarMenuItem, emptyProgressBarMenuItem, backItem, nil];
         [menu alignItemsVertically];
         
-        [self addChild:menu];
-        
         menu.position = ccp(self.contentSize.width/2, self.contentSize.height/2);
         
-//        progressBar = [MGWUProgressBar progressBarWithStyle:MGWUProgressBarStyleHorizontal fillingColor:ccc4f(255, 0, 0, 255)];
-        
-//        progressBar = [MGWUProgressBar progressBarWithStyle:MGWUProgressBarStyleHorizontal fillingImage:@"notification-box.png" capInsets:CGRectMake(1, 1, 13, 48)];
-        
-        progressBar = [MGWUProgressBar progressBarWithStyle:MGWUProgressBarStyleHorizontal revealingSprite:@"metercolors.png"];
-        
-        progressBar.position = ccp(50, (self.contentSize.height/3) * 2);
-//        progressBar.maximumSize = CGSizeMake(self.contentSize.width - 2 * progressBar.position.x, 50);
-        progressBar.maximumValue = 100.f;
-        progressBar.changeValueAnimated = TRUE;
-        
-        [self addChild:progressBar];
+        [self addChild:menu];
     }
     
     return self;
@@ -54,11 +68,15 @@
 
 - (void)fillProgressBar
 {
-    progressBar.currentValue = (arc4random()%100) * 1.f;
+    progressBar1.currentValue = (arc4random()%100) * 1.f;
+    progressBar2.currentValue = (arc4random()%100) * 1.f;
+    progressBar3.currentValue = (arc4random()%100) * 1.f;
 }
 
 - (void)emptyProgressBar {
-    progressBar.currentValue = 0.f;
+    progressBar1.currentValue = 0.f;
+    progressBar2.currentValue = 0.f;
+    progressBar3.currentValue = 0.f;
 }
 
 - (void)back
